31d4fd8bf03d53b90e73f6f5b35f6cd6d659ee3b
Simon Hoefling <simon@hoeflia.eu>
2023-09-13 15:02:23 +0700

    Added icons and imported them

---
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './WeatherApp.css'
+
+import search_icon from '../Assets/search.png'
+import clear_icon from '../Assets/clear.png'
+import cloudy_icon from '../Assets/cloud.png'
+import drizzle_icon from '../Assets/drizzle.png'
+import rain_icon from '../Assets/rain.png'
+import snow_icon from '../Assets/snow.png'
+import wind_icon from '../Assets/wind.png'
+import humidity_icon from '../Assets/humidity.png'
+
+function WeatherApp() {
+  return (
+    <div>WeatherApp</div>
+  )
+}
+
+export default WeatherApp
